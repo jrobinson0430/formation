9c7e6e410f81d67df5ade1194b9f5d0b1baba016
Jacob Robinson <jacob.robinson0430@gmail.com>
2024-01-08 01:59:42 -0500

    trees and backtracking problems

--- sorting_algorithms/insertion.js
+++ sorting_algorithms/insertion.js
@@ -27,3 +27,34 @@ function insertionSort(arr) {
   }
   return arr;
 }
+
+
+function selectionSortRec(arr, start = 0) {
+  // If there is only one element to be sorted, there is
+  // nothing to do.
+  if (start > arr.length - 1) {
+    return arr;
+  }
+
+  // Use recursion to find the end.
+  selectionSortRec(arr, start + 1);
+
+  // Recursive function to find the index of the largest value
+  // from the start of the array (start at zero) up to index last.
+  function indexOfLargest(last, start = 0, largestValueIndex = 0) {
+    if (start > last) {
+      return largestValueIndex;
+    }
+    return indexOfLargest(
+      last,
+      start + 1,
+      arr[largestValueIndex] < arr[start] ? start : largestValueIndex
+    );
+  }
+
+  const swapWith = indexOfLargest(start);
+  // Swap the current value with the one identified as largest
+  [arr[start], arr[swapWith]] = [arr[swapWith], arr[start]];
+
+  return arr;
+} 
