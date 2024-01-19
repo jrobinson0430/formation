ec9de5309e82293b6f9254ff057bb3351764d092
Jacob Robinson <jacob.robinson0430@gmail.com>
2024-01-19 17:49:11 -0500

    more backtracking

--- backtracking_problems/letterCombinations.js
+++ backtracking_problems/letterCombinations.js
@@ -12,16 +12,7 @@ Given a string containing digits from 2-9 inclusive, return all possible letter 
 A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
  */
 var letterCombinations = function (digits) {
-  /*
-    - we need a dictionary to look up the possible letters for each number 2: [a,b,c]
-        - key: digit
-        - value: array representing possible characters 
-    - 
 
-
-    Input: digits = "23"
-    Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
-    */
   if (!digits.length) return [];
   const allStrings = [];
   const map = {
